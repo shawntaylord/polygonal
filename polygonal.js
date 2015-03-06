c612231cb0d675d0a9744933965007852174857a
Shawn Taylor <shawn.b.taylor23@gmail.com>
2015-03-05 22:59:55 -0800

    Made Delaunay triangles

--- polygonal.js
+++ polygonal.js
@@ -4,10 +4,10 @@
     this.canvas = document.getElementById('imageDisplay');
     this.context = this.canvas.getContext('2d');
     this.MAX_NUM_POINTS; // use for calculated range
-    this.num_points = 200;
+    this.num_points = 50;
     this.BASE_X = 2;
     this.BASE_Y = 3;
-    this.PADDING = 200;
+    this.PADDING = 250;
   }
 
   Polygonal.prototype.initialize = function() {
@@ -15,10 +15,32 @@
   }
 
   Polygonal.prototype.polygonize = function() {
-    setPoints.call(this);
-    //createDelaunayTriangles();
-    //setColors();
-    //emphasize();
+    // setPoints returns an Array of Arrays with x, y coordinates
+    var points = setPoints.call(this);
+    // createDelaunayTriangles returns an Array of triplets, where the values
+    // represent indices in 'points'
+    var triangles = createDelaunayTriangles.call(this, points);
+  }
+
+  // This should also fill them. Aaaaand randomly change the colors...
+  function createDelaunayTriangles(points) {
+    var triangles = Delaunay.triangulate(points);
+    // Draw lines between points
+    var low = 0,
+        high = 3;
+    this.context.beginPath();
+    for (high; high < triangles.length; high+=3) {
+      var tr = triangles.slice(low, high);
+      var i = 1;
+      this.context.moveTo(points[tr[0]][0], points[tr[0]][1]);
+      for (i; i < tr.length; i++) {
+        this.context.lineTo(points[tr[i]][0], points[tr[i]][1]);
+      }
+      this.context.lineTo(points[tr[0]][0], points[tr[0]][1]);
+      low = high;
+    }
+    this.context.fill();
+    return triangles;
   }
 
   function initializeEvents() {
@@ -33,12 +55,12 @@
             _.canvas.height = img.height;
             _.MAX_NUM_POINTS = img.width * img.height;
             _.context.drawImage(img, 0, 0);
-            setPoints.call(_);
           }
           img.src = event.target.result;
       }
       reader.readAsDataURL(e.target.files[0]);
     }
+    console.log('finishing initializeEvents');
   }
 
   function halton(index, base) {
@@ -57,11 +79,14 @@
 
   function setPoints() {
     var x,y;
+    var pointsArray = [];
     for (var i = 1; i <= this.num_points; i++) {
       x = halton(i, this.BASE_X) * (this.canvas.width + this.PADDING) - this.PADDING/2;
       y = halton(i, this.BASE_Y) * (this.canvas.height + this.PADDING) - this.PADDING/2;
       fadeInRectangle.call(this, x, y, 3, 3, 0);
+      pointsArray.push([x,y]);
     }
+    return pointsArray;
   }
 
   function fadeInRectangle(x, y, w, h, r, g, b) {
@@ -88,6 +113,10 @@
 window.onload = function() {
   var ply = new Polygonal();
   ply.initialize();
+  var manualStart = document.getElementById('manual-start');
+  manualStart.onclick = function() {
+    ply.polygonize();
+  }
 
   // Give user input to pick number of points and possibly
   // percent darkened? User must hit another button to run
